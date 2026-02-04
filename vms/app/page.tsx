09a858e81d1d262c54e9c92352ab570180ed6873
shoon0615 <shoon0615@github.com>
2026-02-05 01:52:08 +0900

    [vms] nextjs 테스트

---
+++ vms/app/page.tsx
@@ -0,0 +1,9 @@
+import { ComponentExample } from "@/components/preview";
+import { GithubBlock } from "@/components/github";
+
+import { AlertExample } from "@/components/alert-example";
+import { SidebarExample } from "@/components/sidebar-example";
+
+export default function Page() {
+  return <GithubBlock />;
+}
