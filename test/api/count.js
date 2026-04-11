2d35b406b9aa871de48c6c3bf8322617a6c6ebb4
shoon0615 <shoon0615@github.com>
2026-04-12 04:23:27 +0900

    [test] Git 충돌 테스트

--- test/api/count.js
+++ test/api/count.js
@@ -1,9 +1,5 @@
 export let count = 0;
 
-export function increase(val = 1) {
-  return (count += val);
-}
-
-export function decrease() {
-  return (count -= 1);
+export function increase() {
+  return (count += 1);
 }
